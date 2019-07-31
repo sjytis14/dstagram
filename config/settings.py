93287c67deb5148f729deb3d600e4ae5d88c9498
seojeong <sjytis14@naver.com>
2019-07-31 20:52:14 +0900

    final__

--- config/settings.py
+++ config/settings.py
@@ -128,13 +128,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "web_staticfiles")
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 LOGIN_REDIRECT_URL = '/'
 
